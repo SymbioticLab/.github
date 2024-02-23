b6978cf641fa16596dd792d1a1f604ff7d77361c
Jae-Won Chung <jwnchung@umich.edu>
2024-02-22 20:43:19 -0500

    Fix gen_repo_card_html.py

--- profile/gen_repo_card_html.py
+++ profile/gen_repo_card_html.py
@@ -1,13 +1,12 @@
 import sys
 
-HTML_TEMPLATE = r"""<a href="https://github.com/ml-energy/zeus">
+HTML_TEMPLATE = r"""<a href="https://github.com/{username}/{repo}">
   <picture>
     <source media="(prefers-color-scheme: dark)" srcset="https://github-readme-stats-git-master-jaywonchungs-projects.vercel.app/api/pin/?username={username}&repo={repo}&theme=github_dark&description_lines_count=2&show_owner=false&border_color=30363d">
     <source media="(prefers-color-scheme: light)" srcset="https://github-readme-stats-git-master-jaywonchungs-projects.vercel.app/api/pin/?username={username}&repo={repo}&theme=default_repocard&description_lines_count=2&show_owner=false&border_color=d0d7de">
     <img src="https://github-readme-stats-git-master-jaywonchungs-projects.vercel.app/api/pin/?username={username}&repo={repo}&theme=default_repocard&description_lines_count=2&show_owner=false&border_color=d0d7de" alt="{repo} repo card">
   </picture>
-</a>
-"""
+</a>"""
 
 
 def main():
